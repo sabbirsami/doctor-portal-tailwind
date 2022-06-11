9e738c33563d6f8a5aeb1815ae7544755801edb0
SabbirSami <smd71430@gmail.com>
2022-06-11 12:14:49 +0600

    fix Primary button

--- src/Pages/Shared/PrimaryButton.js
+++ src/Pages/Shared/PrimaryButton.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { IoIosArrowDropright } from "react-icons/io";
 import arrow from "../../assets/icons/icons8-arrow-24.png";
 
 const PrimaryButton = () => {
     return (
-        <button className="btn btn-primary bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-blue-500 hover:to-cyan-500 px-8 rounded-full text-white shadow-2xl">
-            Get Started <img className="pl-3 color-white" src={arrow} alt="" />
+        <button className="btn btn-outline btn-success px-8 rounded-full shadow-2xl">
+            Get Started{" "}
+            <IoIosArrowDropright className="text-xl ml-4"></IoIosArrowDropright>
         </button>
     );
 };
