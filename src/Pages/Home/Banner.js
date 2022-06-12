98ec27e1fa9a21423ce3d9bd01eda1b0098f861c
SabbirSami <smd71430@gmail.com>
2022-06-12 18:31:27 +0600

    fix bannar section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,11 +4,33 @@ import PrimaryButton from "../Shared/PrimaryButton";
 
 const Banner = () => {
     return (
-        <div>
-            <div className="hero min-h-[60vh] ">
+        <div className="container mx-auto">
+            <div className="hero lg:min-h-[85vh] ">
                 <div className="hero-content text-center flex-col lg:flex-row-reverse">
                     <div>
-                        <h1 className="text-6xl font-bold">
+                        <div className="mx-auto text-center py-5">
+                            {/* <div class="alert mx-auto border-solid border-2 p-2 border-sky-500 bg-white  shadow-lg w-96">
+                                <div>
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="stroke-current flex-shrink-0 h-6 w-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        />
+                                    </svg>
+                                    <span>
+                                        Your purchase has been confirmed!
+                                    </span>
+                                </div>
+                            </div> */}
+                        </div>
+                        <h1 className="lg:text-9xl lg:px-28 text-6xl font-bold">
                             Your New Smile Starts Here
                         </h1>
                         <p className="py-6 lg:px-48 text-xl">
