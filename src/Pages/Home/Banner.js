55bd38c96e3882776eee69849de42b08beec6599
SabbirSami <smd71430@gmail.com>
2022-06-19 16:58:22 +0600

    fix text

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="hero lg:min-h-[88vh] ">
                 <div className="hero-content text-center flex-col lg:flex-row-reverse">
                     <div>
-                        <div className="mx-auto text-center py-5">
+                        <div className="mx-auto text-center">
                             {/* <div class="alert mx-auto border-solid border-2 p-2 border-sky-500 bg-white  shadow-lg w-96">
                                 <div>
                                     <svg
@@ -34,12 +34,13 @@ const Banner = () => {
                         <h1 className="lg:text-9xl lg:px-28 text-6xl font-bold">
                             Your New Smile Starts Here
                         </h1>
-                        <p className="py-6 lg:px-48 text-xl">
-                            Provident cupiditate voluptatem et in. Quaerat
-                            fugiat ut assumenda excepturi exercitationem quasi.
-                            In deleniti eaque aut repudiandae et a id nisi.
+                        <p className="py-6 text-2xl">
+                            Provident cupiditate voluptatem et in. Recusandae,
+                            voluptates quo incidunt harum minus mollitia!
+                            Quaerat assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
                         </p>
-                        <button className="btn btn-outline border-2  mr-4 border-sky-200 px-8 rounded-full shadow-2xl">
+                        <button className="btn btn-outline border-2 bg-blue-600 text-white mr-4 border-sky-200 px-8 rounded-full shadow-2xl">
                             Contact{" "}
                             {/* <IoIosArrowDropright className="text-xl ml-4"></IoIosArrowDropright> */}
                         </button>
--- src/Pages/Shared/PrimaryButton.js
+++ src/Pages/Shared/PrimaryButton.js
@@ -5,7 +5,7 @@ import arrow from "../../assets/icons/icons8-arrow-24.png";
 const PrimaryButton = () => {
     return (
         <button className="btn btn-outline border-2 border-sky-500 px-8 rounded-full shadow-2xl">
-            Get Started{" "}
+            Get Started
             <IoIosArrowDropright className="text-xl ml-4"></IoIosArrowDropright>
         </button>
     );
