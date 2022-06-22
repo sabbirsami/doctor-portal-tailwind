afd78905c34fadc9520e88df7b458448ff96d3f1
SabbirSami <smd71430@gmail.com>
2022-06-22 15:38:28 +0600

    Banner section done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -10,15 +10,17 @@ const Banner = () => {
                 <div className="hero lg:min-h-[100vh] text-white">
                     <div className="hero-content mr-auto">
                         <div className="text-left"></div>
-                        <div className="pb-44">
+                        <div className="lg:pb-44 py-20">
                             <p className=" text-2xl">Creating</p>
                             <h1 className="pb-8 lg:text-8xl text-6xl font-bold">
                                 Brighter Smiles
                             </h1>
-                            <button className="btn btn-outline border-2 bg-emerald-500 text-white mr-4 border-sky-200 px-8 rounded-0 shadow-2xl">
-                                Contact{" "}
-                            </button>
-                            <PrimaryButton></PrimaryButton>{" "}
+                            <div className="">
+                                <button className="btn lg:inline block btn-outline border-2 bg-emerald-500 text-white mr-4 border-sky-200 px-8 mb-2 rounded-0 shadow-2xl">
+                                    Contact{" "}
+                                </button>
+                                <PrimaryButton></PrimaryButton>{" "}
+                            </div>
                         </div>
                     </div>
                 </div>
