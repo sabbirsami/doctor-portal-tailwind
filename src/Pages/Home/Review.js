feeaccc5690e89493ef62ee881fc6c10fdfb785c
SabbirSami <smd71430@gmail.com>
2022-06-22 15:07:13 +0600

    fix review

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -4,10 +4,7 @@ const Review = ({ review }) => {
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl justify-between">
             <div className="card-body">
-                <p className="py-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Recusandae, voluptates quo incidunt harum minus mollitia!
-                </p>
+                <p className="py-5">{review.review}</p>
                 <div className="flex items-center">
                     <div className="avatar">
                         <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
