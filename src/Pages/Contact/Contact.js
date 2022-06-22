0a950ab71297656d33d676d83251899d95a88761
SabbirSami <smd71430@gmail.com>
2022-06-22 14:42:53 +0600

    apply email.js in contact page done

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,55 +1,88 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { MdLocationPin } from "react-icons/md";
 import { IoMdCall } from "react-icons/io";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_rwlct1p",
+                "template_17wglbe",
+                form.current,
+                "qdhDVcg6u8gZ0Y4y5"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.target.reset();
+    };
     return (
         <div>
             <div className="container mx-auto">
                 <div class="hero lg:min-h-[70vh]">
                     <div class="hero-content flex-col lg:flex-row-reverse">
                         <div class="card lg:order-first order-last flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
-                            <div class="card-body">
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">
-                                            Your email address
-                                        </span>
-                                    </label>
-                                    <input
-                                        type="email"
-                                        placeholder="Email"
-                                        class="input input-bordered"
-                                    />
-                                </div>
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">
-                                            Your name/ company's name
-                                        </span>
-                                    </label>
-                                    <input
-                                        type="text"
-                                        placeholder="Name"
-                                        class="input input-bordered"
-                                    />
-                                </div>
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">Message</span>
-                                    </label>
-                                    <textarea
-                                        class="textarea textarea-bordered h-24"
-                                        placeholder="Write your massage here..."
-                                    ></textarea>
-                                </div>
+                            <form ref={form} onSubmit={sendEmail}>
+                                <div class="card-body">
+                                    <div class="form-control">
+                                        <label class="label">
+                                            <span class="label-text">
+                                                Your email address
+                                            </span>
+                                        </label>
+                                        <input
+                                            name="email"
+                                            type="email"
+                                            placeholder="Email"
+                                            class="input input-bordered"
+                                        />
+                                    </div>
+                                    <div class="form-control">
+                                        <label class="label">
+                                            <span class="label-text">
+                                                Your name/ company's name
+                                            </span>
+                                        </label>
+                                        <input
+                                            name="name"
+                                            type="text"
+                                            placeholder="Name"
+                                            class="input input-bordered"
+                                        />
+                                    </div>
+                                    <div class="form-control">
+                                        <label class="label">
+                                            <span class="label-text">
+                                                Message
+                                            </span>
+                                        </label>
+                                        <textarea
+                                            name="message"
+                                            class="textarea textarea-bordered h-24"
+                                            placeholder="Write your massage here..."
+                                        ></textarea>
+                                    </div>
 
-                                <div class="form-control mt-6">
-                                    <button class="btn btn-primary">
-                                        Send
-                                    </button>
+                                    <div class="form-control mt-6">
+                                        <button
+                                            type="submit"
+                                            class="btn btn-primary"
+                                        >
+                                            Send
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                         <div class="text-center lg:text-left pr-22">
                             <h1 class="lg:text-5xl text-4xl pb-4">
