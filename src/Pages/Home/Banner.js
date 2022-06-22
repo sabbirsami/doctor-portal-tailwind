4c654f3d2900bc6ce36cc86f91b177ce6d330e2b
SabbirSami <smd71430@gmail.com>
2022-06-22 12:21:44 +0600

    add review section

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,12 +5,13 @@ import PrimaryButton from "../Shared/PrimaryButton";
 
 const Banner = () => {
     return (
-        <div className="container mx-auto">
-            <div className="hero lg:min-h-[88vh] ">
-                <div className="hero-content text-center flex-col lg:flex-row-reverse">
-                    <div>
-                        <div className="mx-auto text-center">
-                            {/* <div class="alert mx-auto border-solid border-2 p-2 border-sky-500 bg-white  shadow-lg w-96">
+        <div className="">
+            <div className="container mx-auto">
+                <div className="hero lg:min-h-[88vh] ">
+                    <div className="hero-content text-center flex-col lg:flex-row-reverse">
+                        <div>
+                            <div className="mx-auto text-center">
+                                {/* <div class="alert mx-auto border-solid border-2 p-2 border-sky-500 bg-white  shadow-lg w-96">
                                 <div>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
@@ -30,21 +31,23 @@ const Banner = () => {
                                     </span>
                                 </div>
                             </div> */}
+                            </div>
+                            <h1 className="lg:text-9xl lg:px-28 text-6xl font-bold">
+                                Your New Smile Starts Here
+                            </h1>
+                            <p className="py-6 text-2xl">
+                                Provident cupiditate voluptatem et in.
+                                Recusandae, voluptates quo incidunt harum minus
+                                mollitia! Quaerat assumenda excepturi
+                                exercitationem quasi. In deleniti eaque aut
+                                repudiandae et a id nisi.
+                            </p>
+                            <button className="btn btn-outline border-2 bg-blue-600 text-white mr-4 border-sky-200 px-8 rounded-full shadow-2xl">
+                                Contact{" "}
+                                {/* <IoIosArrowDropright className="text-xl ml-4"></IoIosArrowDropright> */}
+                            </button>
+                            <PrimaryButton></PrimaryButton>{" "}
                         </div>
-                        <h1 className="lg:text-9xl lg:px-28 text-6xl font-bold">
-                            Your New Smile Starts Here
-                        </h1>
-                        <p className="py-6 text-2xl">
-                            Provident cupiditate voluptatem et in. Recusandae,
-                            voluptates quo incidunt harum minus mollitia!
-                            Quaerat assumenda excepturi exercitationem quasi. In
-                            deleniti eaque aut repudiandae et a id nisi.
-                        </p>
-                        <button className="btn btn-outline border-2 bg-blue-600 text-white mr-4 border-sky-200 px-8 rounded-full shadow-2xl">
-                            Contact{" "}
-                            {/* <IoIosArrowDropright className="text-xl ml-4"></IoIosArrowDropright> */}
-                        </button>
-                        <PrimaryButton></PrimaryButton>{" "}
                     </div>
                 </div>
             </div>
