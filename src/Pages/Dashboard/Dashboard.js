3cf7b51e9dbed45b1fd237965ddc9b08af95741b
SabbirSami <smd71430@gmail.com>
2022-06-23 23:39:45 +0600

    set deshboard route for admin

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,15 +2,16 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, Outlet } from "react-router-dom";
 import auth from "../../firebase.init";
+import useAdmin from "../../hooks/useAdmin";
 import Loading from "../Shared/Loading";
 
 const Dashboard = () => {
     const [user, loading, error] = useAuthState(auth);
-
+    const [admin] = useAdmin(user);
     if (loading) {
         return <Loading></Loading>;
     }
-    console.log(user);
+
     return (
         <div className="drawer drawer-mobile">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -39,15 +40,24 @@ const Dashboard = () => {
                     <li>
                         <Link to="/dashboard/review">Review</Link>
                     </li>
-                    <li>
-                        <Link to="/dashboard/users">Users</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard/addDoctor">Add Doctor</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard/manageDoctor">Manage Doctor</Link>
-                    </li>
+
+                    {admin && (
+                        <>
+                            <li>
+                                <Link to="/dashboard/users">Users</Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard/addDoctor">
+                                    Add Doctor
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard/manageDoctor">
+                                    Manage Doctor
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </div>
         </div>
